2ee094f61b791883e88cbb628657eb1d1bb3a003
codingshot <45281667+codingshot@users.noreply.github.com>
2024-09-30 23:08:36 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
   const nextConfig = {
     reactStrictMode: true,
+    images: {
+      domains: ['pbs.twimg.com', 'activity.community'],
+    },
     
   }
-
-  module.exports = nextConfig
